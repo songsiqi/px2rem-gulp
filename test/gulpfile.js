11ec0e7c5f3734f0d77248959374ddc711069f87
songsiqi <songsiqi2006@126.com>
2015-04-03 12:16:27 +0800

    migrate to github

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -4,7 +4,7 @@ var px2rem = require('../index');
 gulp.task('px2rem-1', function() {
     gulp.src('./src/**/*.css')
         .pipe(px2rem())
-        .pipe(gulp.dest('./src'));
+        .pipe(gulp.dest('src'));
 });
 
 gulp.task('px2rem-2', function() {
